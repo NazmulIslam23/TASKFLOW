928910ba0450f4ab96a966f77e8b4773f9455cdf
Nazmul <najmulhasanhimu7@gmail.com>
2026-05-01 21:50:52 +0600

    set routing

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const crud=express();
 const Port=3000;
 
 crud.use(express.json());
+const taskRoutes=require("./routes/taskRoutes");
+crud.use("/tasks",taskRoutes);
+crud.get("/testing",(req,res)=>
+{
+    res.send("API is working");
+});
 
 crud.listen(Port,()=>
 {
     console.log(`Server running port ${Port}`);
 });
-crud.get("/testing",(req,res)=>
-{
-    res.send("API is working");
-});
+
