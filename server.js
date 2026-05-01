0ac4276471968be4e5aa8af04e62796672349c80
Nazmul <najmulhasanhimu7@gmail.com>
2026-05-01 21:30:23 +0600

    API testing

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express=require("express");
 const crud=express();
 const Port=3000;
-const taskRoutes=require("./routes/taskRoutes");
 
 crud.use(express.json());
-crud.use("/tasks",taskRoutes);
 
 crud.listen(Port,()=>
 {
     console.log(`Server running port ${Port}`);
+});
+crud.get("/testing",(req,res)=>
+{
+    res.send("API is working");
 });
